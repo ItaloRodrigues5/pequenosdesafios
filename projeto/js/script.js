6f11a2f57e7dfdd177e481334baf305db89d3493
Italo Ferreira <todorokirerojob@gmail.com>
2024-11-13 18:30:30 -0200

    Adicionando no repositório por meio do Git terminal VS Code

---
+++ projeto/js/script.js
@@ -0,0 +1,24 @@
+document.getElementById('submitBtn').addEventListener('click', function(){
+    const email = document.getElementById('email').value.trim();
+    const senha = document.getElementById('senha').value.trim();
+
+    if(!email || !senha){
+        alert('Preencha os campos vazios.');
+        return;
+    }
+
+    if(!email.includes('@') || !email.includes('.')){
+        alert('Por favor, preencha no formato de um e-mail incluindo "@" e "."');
+        return;
+    }
+
+    //Imprimindo a mensagem de sucesso do login
+    const responseMessage = document.getElementById('responseMessage');
+    responseMessage.textContent = 'Login preenchido com sucesso';
+    responseMessage.style.color = 'green';
+
+    //Limpando os campos
+    document.getElementById('email').value = '';
+    document.getElementById('senha').value = '';
+
+})
