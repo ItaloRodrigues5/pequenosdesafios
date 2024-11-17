61a7a57ac3a6f15cf813a3c1f69947763293be7a
Italo Ferreira <todorokirerojob@gmail.com>
2024-11-17 00:51:58 -0200

    Adicionando teste de inputs
    Testando com JavaScript se os inputs estão preenchidos ou falta algum requisito obrigatório. Ainda só na tela de Login. Será feita a transferência para as demais telas de formulário amanhã.

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -1,37 +1,3 @@
-// document.getElementById('submitBtn').addEventListener('click', function(event){
-    
-//     event.preventDefault();
-    
-//     const email = document.getElementById('email').value.trim();
-//     const senha = document.getElementById('senha').value.trim();
-
-//     if(!email || !senha){
-//         alert('Preencha os campos vazios.');
-//         return;
-//     }
-//     else{
-//         document.getElementById('userform').submit();
-//     }
-
-//     if(!email.includes('@') && !email.includes('.')){
-//         alert('Por favor, preencha no formato de um e-mail incluindo "@" e "."');
-//         return;
-//     }
-//     else{
-//         document.getElementById('userform').submit();
-//     }
-
-//     //Imprimindo a mensagem de sucesso do login
-//     const responseMessage = document.getElementById('responseMessage');
-//     responseMessage.textContent = 'Login preenchido com sucesso';
-//     responseMessage.style.color = 'green';
-
-//     //Limpando os campos
-//     document.getElementById('email').value = '';
-//     document.getElementById('senha').value = '';
-
-// });
-
 document.getElementById('submitBtn').addEventListener('click', function(event){
     
     event.preventDefault();
