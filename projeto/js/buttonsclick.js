45234027cb6a6c512a149dcf372e6912b235896c
Italo Ferreira <todorokirerojob@gmail.com>
2024-11-16 22:13:33 -0200

    Adição de JavaScript para manipular Ion Icon e áudio em botão
    Seguintes telas alteradas implementando mudanças no CSS e JavaScript: selectjogosF1, selectjogosF2, nivel

---
+++ projeto/js/buttonsclick.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const button = document.getElementById('audiobutton');
+    const icon = document.getElementById('volumeicon');
+    const audioPlayer = document.getElementById('audioplayer');
+
+    audioPlayer.loop = true;
+    
+    button.addEventListener('click', () => {
+        if(icon.getAttribute('name') == 'volume-high'){
+            icon.setAttribute('name', 'volume-mute');
+            audioPlayer.pause();
+            console.log('ícone mudado para ficar sem som');
+        }
+        else{
+            icon.setAttribute('name', 'volume-high');
+            audioPlayer.play();
+            console.log('Ícone mudado para ter som');
+        }
+    });
+
+});
